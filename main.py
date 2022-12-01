c1b4123a5fcab478f81d0a40758ee0e21b9ed570
joshuamilkman <113635755+joshuamilkman@users.noreply.github.com>
2022-12-01 15:49:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from train import *
 
 
 def print_hi(name):
@@ -11,6 +8,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("hello, welcome to the best plagarism checker")
+    
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
